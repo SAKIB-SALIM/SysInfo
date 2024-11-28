37d9fc3f8d762ae656005a51df4c5a9b8048c191
SAKIB-SALIM <salimsakib775@gmail.com>
2024-11-28 10:55:09 +0600

    1st

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"main.go/modules/system"
-	"main.go/webhooks" // Replace "your_project" with the actual module name if different
 )
 
 func main() {
-	for _, webhook := range webhooks.Webhooks {
-		system.Run(webhook.URL)
-	}
+		system.Run()
 }
